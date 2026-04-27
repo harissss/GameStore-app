e5952fd0a9b75d5443de0d68ceae5cb3dc7341f9
Haris <au755228@uni.au.dk>
2026-04-27 18:09:14 +0200

    Added a search route, so that you can search for games. Didn't implement the controller fully.

--- backend/src/controllers/gamesController.js
+++ backend/src/controllers/gamesController.js
@@ -1,17 +1,77 @@
+import Game from "../models/Game.js";
+
 // This is where the backend functionality is implemented. Everything related to the games is handled here. 
 // These functions are invoked from the gamesRoutes.js file. 
 
 // Function to get all the available games from the database, so that it can be shown in the UI. 
-export function getAllGames(req, res) {
+// Async because there will be a Promise we have to await. 
+export async function getAllGames(req, res) {
+    // Steps: 
+    // - Retrieve all games from the MongoDB
+    // - Encode them all as JSON (i think already done?)
+    // - Send them in the response
+    // - Set status to 200 (request succeeded)
+    // - Handel potential errors, and send relevant error codes
+
+    try {
+        const games = await Game.find();
+        res.status(200).json(games);
+    } catch (err) {
+        //status code 500: internal server error
+        console.error("Error in getAllGames controller:", err);
+        res.status(500).json({message:"Could not fetch games from the database."});
+    }
+    
+
     res.status(200).json({message:"Games sent"});
 }
 
 // Function to show the store page of a specific game, when the user clicks on the game. 
 export function getStorePage(req, res) {
-    res.status(200).json({message:"This is the store page for game with given id"})
+    // Steps: 
+    // - Get the id somehow
+    // - Query the MongoDB for the specific game, and relevant information for the store page
+    // - Encode as JSON 
+    // - Send response with status 200 
+    // - Handel errors and send relevant error codes
+
+    let id = req.params.id;
+
+    res.status(200).json({message:`This is the store page for game with id: ${id}`});
 }
 
 // Function to add a game to a user's "favorites list". This happens when the user clicks the "like" button on a game. 
 export function addToFavorites(req, res) {
-    res.status(201).json({message:"Game added to favorites"})
+    // Steps: 
+    // - Get id of game
+    // - Get id of user 
+    // - Add it to the list of the user's favorites
+    // - Send response with status 200/201 (idk which is better)
+    // - Should anything be sent in the JSON response?
+    // - Handle errors and send relevant error codes 
+
+    let id = req.params.id;
+
+    res.status(201).json({message:`Game (id: ${id}) added to favorites`});
+}
+
+export async function searchGames(req, res) {
+    // Notes: 
+    // The client can send something like "GET /search?terms=halo&genre=action"
+    // Then req.query will have the two fields: terms, genre. 
+    // These can be accessed as shown below
+    let terms = req.query.terms;
+    let genres = req.query.genres;
+
+    try {
+        const matchingGames = await Game.find(/* CONSTRUCT THE QUERY FILTER */);
+        // HANDLE 404 NOT FOUND STUFF?
+        res.status(200).json(matchingGames);
+    } catch (err) {
+        //status code 500: internal server error
+        console.error("Error in searchGames controller:", err);
+        res.status(500).json({message:"Could not fetch games from the database."});
+    }
+
+    res.status(200).json({message:`Games matching search query: _${terms}_ with genres: _${genres}_ sent`});
 }
--- backend/src/routes/gamesRoutes.js
+++ backend/src/routes/gamesRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
-import { getAllGames, getStorePage, addToFavorites } from "../controllers/gamesController.js";
+import { getAllGames, getStorePage, addToFavorites, searchGames } from "../controllers/gamesController.js";
 
 // Make a router, using express js 
 const router = express.Router();
 
 // Create all the routes. 
 // All the routes will call a function, where logic is implemented
-router.get("/", getAllGames)
-router.get("/:id", getStorePage)
-router.put("/:id", addToFavorites)
+router.get("/", getAllGames);
+router.get("/search", searchGames);
+router.get("/:id", getStorePage);
+router.put("/:id", addToFavorites);
+
 
 export default router;
